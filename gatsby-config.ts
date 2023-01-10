5eab06a929466c595d4008b508d792e1fcdc9f38
James Anderson <jamesanderson.contact@gmail.com>
2023-01-10 12:12:41 +1100

    remove existing content

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,56 +1,72 @@
-import type { GatsbyConfig } from "gatsby";
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
+import type { GatsbyConfig } from 'gatsby';
 
 dotenv.config({
-  path: `.env.${process.env.NODE_ENV}`,
+    path: `.env.${process.env.NODE_ENV}`,
 });
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    description: `I'm a motion graphics artist, web designer and illustrator based in Sydney, Australia.`,
-    siteUrl: `https://jamesandersondesign.com.au/`,
-    title: `James Anderson - Product Designer and Web Developer`,
-  },
-  graphqlTypegen: true,
-  plugins: [
-    "gatsby-plugin-sass",
-    "gatsby-plugin-image",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
+    graphqlTypegen: true,
+    plugins: [
+        'gatsby-plugin-sass',
+        'gatsby-plugin-image',
+        'gatsby-plugin-sitemap',
+        'gatsby-plugin-mdx',
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp',
+        {
+            __key: 'images',
+            options: {
+                name: 'images',
+                path: './src/images/',
+            },
+            resolve: 'gatsby-source-filesystem',
+        },
+        {
+            __key: 'animations',
+            options: {
+                name: 'animations',
+                path: './src/animations/',
+            },
+            resolve: 'gatsby-source-filesystem',
+        },
+        {
+            __key: 'icons',
+            options: {
+                name: 'icons',
+                path: './src/icons/',
+            },
+            resolve: 'gatsby-source-filesystem',
+        },
+        {
+            __key: 'pages',
+            options: {
+                name: 'pages',
+                path: './src/pages/',
+            },
+            resolve: 'gatsby-source-filesystem',
+        },
 
-    {
-      options: {
-        gtagConfig: {
-          anonymize_ip: true,
-          cookie_expires: 0,
+        {
+            options: {
+                gtagConfig: {
+                    anonymize_ip: true,
+                    cookie_expires: 0,
+                },
+                pluginConfig: {
+                    head: false,
+                    respectDNT: true,
+                },
+                trackingIds: [process.env.GA_TRACKING_ID],
+            },
+            resolve: `gatsby-plugin-google-gtag`,
         },
-        pluginConfig: {
-          head: false,
-          respectDNT: true,
-        },
-        trackingIds: [process.env.GA_TRACKING_ID],
-      },
-      resolve: `gatsby-plugin-google-gtag`,
+    ],
+    siteMetadata: {
+        description: `I'm a motion graphics artist, web designer and illustrator based in Sydney, Australia.`,
+        siteUrl: `https://jamesandersondesign.com.au/`,
+        title: `James Anderson - Product Designer and Web Developer`,
     },
-  ],
 };
 
 export default config;
