006d805ee80039c091156db15f35154c9ad6bec1
James Anderson <jamesanderson.contact@gmail.com>
2023-01-12 18:04:44 +1100

    added isPrivate field to posts

--- src/components/ContentWork/ContentWork.tsx
+++ src/components/ContentWork/ContentWork.tsx
@@ -34,6 +34,7 @@ const ContentWork: FC = () => {
                     tags {
                         tags
                     }
+                    isPrivate
                 }
             }
         }
@@ -121,54 +122,60 @@ const ContentWork: FC = () => {
                         title="Filters"
                     />
                 </div>
-                {filteredPosts?.slice(0, count).map((post, index) => (
-                    <Fragment key={post?.slug}>
-                        <motion.article
-                            initial={{ opacity: 0, y: isMobile ? 0 : 100 }}
-                            transition={{ duration: 1, type: 'spring' }}
-                            viewport={{
-                                once: true,
-                            }}
-                            whileInView={{ opacity: 1, y: 0 }}
-                        >
-                            <a href={post?.slug || ''}></a>
-                            {post?.thumbnail?.gatsbyImageData && (
-                                <GatsbyImage
-                                    alt={post.title || ''}
-                                    className="image"
-                                    image={post.thumbnail.gatsbyImageData}
-                                />
-                            )}
-                            <div className="content">
-                                <ul>
-                                    {post?.tags?.tags?.map((tag, index) => (
-                                        <Fragment key={tag}>
-                                            <li>{tag}</li>
-                                            {index <
-                                            (post.tags?.tags?.length
-                                                ? post.tags?.tags?.length - 1
-                                                : 0) ? (
-                                                <span />
-                                            ) : null}
-                                        </Fragment>
-                                    ))}
-                                </ul>
-                                <h2>{post?.title}</h2>
-                                {post?.description &&
-                                    renderRichText(
-                                        post?.description as any,
-                                        {}
-                                    )}
-                                <span>{'Read more'}</span>
-                            </div>
-                        </motion.article>
-                        {index <
-                        (filteredPosts.slice(0, count || 3) || []).length -
-                            1 ? (
-                            <div className="divider" />
-                        ) : null}
-                    </Fragment>
-                ))}
+                {filteredPosts?.slice(0, count).map((post, index) => {
+                    if (post?.isPrivate) {
+                        return null;
+                    }
+                    return (
+                        <Fragment key={post?.slug}>
+                            <motion.article
+                                initial={{ opacity: 0, y: isMobile ? 0 : 100 }}
+                                transition={{ duration: 1, type: 'spring' }}
+                                viewport={{
+                                    once: true,
+                                }}
+                                whileInView={{ opacity: 1, y: 0 }}
+                            >
+                                <a href={post?.slug || ''}></a>
+                                {post?.thumbnail?.gatsbyImageData && (
+                                    <GatsbyImage
+                                        alt={post.title || ''}
+                                        className="image"
+                                        image={post.thumbnail.gatsbyImageData}
+                                    />
+                                )}
+                                <div className="content">
+                                    <ul>
+                                        {post?.tags?.tags?.map((tag, index) => (
+                                            <Fragment key={tag}>
+                                                <li>{tag}</li>
+                                                {index <
+                                                (post.tags?.tags?.length
+                                                    ? post.tags?.tags?.length -
+                                                      1
+                                                    : 0) ? (
+                                                    <span />
+                                                ) : null}
+                                            </Fragment>
+                                        ))}
+                                    </ul>
+                                    <h2>{post?.title}</h2>
+                                    {post?.description &&
+                                        renderRichText(
+                                            post?.description as any,
+                                            {}
+                                        )}
+                                    <span>{'Read more'}</span>
+                                </div>
+                            </motion.article>
+                            {index <
+                            (filteredPosts.slice(0, count || 3) || []).length -
+                                1 ? (
+                                <div className="divider" />
+                            ) : null}
+                        </Fragment>
+                    );
+                })}
                 {(filteredPosts?.length || 0) > count && (
                     <button onClick={() => setCount(count + (workCount || 3))}>
                         {'Show more'}
